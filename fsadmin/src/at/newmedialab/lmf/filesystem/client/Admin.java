198fd22617839ae31dad5ceaf7dd181db03e77e4
Sebastian Schaffert <sebastian.schaffert@gmail.com>
2011-10-11 15:07:23 +0200

    initial commit

---
+++ fsadmin/src/at/newmedialab/lmf/filesystem/client/Admin.java
@@ -0,0 +1,59 @@
+package at.newmedialab.lmf.filesystem.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+import com.smartgwt.client.types.Side;
+import com.smartgwt.client.widgets.HTMLFlow;
+import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.tab.Tab;
+import com.smartgwt.client.widgets.tab.TabSet;
+
+/**
+ * Add file description here!
+ * <p/>
+ * Author: Sebastian Schaffert
+ */
+public class Admin implements EntryPoint {
+
+    public static final String SERVER_URL="http://localhost:8080/LMF/";
+
+
+    public void onModuleLoad() {
+        createAdminOverview();
+    }
+
+
+
+    public void createAdminOverview() {
+        RootPanel rootPanel = RootPanel.get("adminPanel");
+        rootPanel.clear();
+
+        TabSet tabSet = new TabSet();
+        tabSet.setTabBarPosition(Side.TOP);
+        tabSet.setWidth(800);
+        tabSet.setHeight100();
+
+        Tab tSearch = new Tab("Search");
+        tSearch.setPane(new SearchAdminPanel());
+        tabSet.addTab(tSearch);
+
+        Tab tPath   = new Tab("Path Mappings");
+        tPath.setPane(new PathMappingAdminPanel());
+        tabSet.addTab(tPath);
+
+        rootPanel.add(tabSet);
+
+        /*
+        final VLayout layout = new VLayout(20);
+        layout.setWidth(800);
+        rootPanel.add(layout);
+
+
+        HTMLFlow greeting = new HTMLFlow();
+        greeting.setContents("<h2>Admin Panel (Hello World!!!)</h2>");
+        layout.addMember(greeting);
+
+        layout.draw();
+        */
+    }
+}
---
+++ fsadmin/src/at/newmedialab/lmf/filesystem/client/PathMappingAdminPanel.java
@@ -0,0 +1,14 @@
+package at.newmedialab.lmf.filesystem.client;
+
+import com.smartgwt.client.widgets.layout.VLayout;
+
+/**
+ * Add file description here!
+ * <p/>
+ * Author: Sebastian Schaffert
+ */
+public class PathMappingAdminPanel extends VLayout{
+
+    public PathMappingAdminPanel() {
+    }
+}
